29576b8f3813f6afb859ab31254a926e094f359c
azumateam <ydhvocal@naver.com>
2020-08-01 15:55:21 +0900

    doModify(게시물 수정처리) 작업

--- src/main/java/com/sbs/starter/service/ArticleServiceImpl.java
+++ src/main/java/com/sbs/starter/service/ArticleServiceImpl.java
@@ -46,4 +46,9 @@ public class ArticleServiceImpl implements ArticleService {
 	public void delete(long id) {
 		articleDao.delete(id);
 	}
+
+	@Override
+	public void modify(Map<String, Object> param) {
+		articleDao.modify(param);
+	}
 }
