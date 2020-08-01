eee048abd10657777cbbffe19f1d7fd5aee7fa6a
azumateam <ydhvocal@naver.com>
2020-08-01 16:25:29 +0900

    BeforeActionInterceptor 적용하여, 매번 로그인한 회원의 정보를 DB에서 꺼내오는 작업을 간편화

--- src/main/java/com/sbs/starter/controller/HomeController.java
+++ src/main/java/com/sbs/starter/controller/HomeController.java
@@ -1,13 +1,10 @@
 package com.sbs.starter.controller;
 
-import javax.servlet.http.HttpSession;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.sbs.starter.dto.Member;
 import com.sbs.starter.service.MemberService;
 
 import lombok.extern.slf4j.Slf4j;
@@ -19,17 +16,7 @@ public class HomeController {
 	MemberService memberService;
 
 	@RequestMapping("/home/main")
-	public String showMain(HttpSession session, Model model) {
-		long loginedMemberId = 0;
-
-		if (session.getAttribute("loginedMemberId") != null) {
-			loginedMemberId = (long) session.getAttribute("loginedMemberId");
-		}
-
-		Member loginedMember = memberService.getOne(loginedMemberId);
-
-		model.addAttribute("loginedMember", loginedMember);
-
+	public String showMain(Model model) {
 		return "home/main";
 	}
 
