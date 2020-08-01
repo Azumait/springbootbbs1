48f69ba470ce019c474dcdc23afaf324eddca265
azumateam <ydhvocal@naver.com>
2020-08-01 14:28:39 +0900

    article/list 로 접속하면 list.jsp 나오게 하기

---
+++ src/main/java/com/sbs/starter/controller/ArticleController.java
@@ -0,0 +1,12 @@
+package com.sbs.starter.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ArticleController {
+	@RequestMapping("/article/list")
+	public String showList() {
+		return "article/list";
+	}
+}
